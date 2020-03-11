d2dd5631a9920574a281e7d3e11e28ec54901ccb
KSentak <k.sentak@outlook.com>
2020-03-11 15:06:04 -0400

    Created Pokecard component and linked it to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
---
+++ src/Pokecard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The name is Pokemon</h1>
+        <h2>Pickachu</h2>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
