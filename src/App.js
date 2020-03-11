4d83cd8b0447197d3b316a5fb231f4b291056a36
KSentak <k.sentak@outlook.com>
2020-03-11 15:22:21 -0400

    Finished a single Pokecard component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Pokecard id={4} name='Charmander' type='fire' exp={62} />
+    </div>
+  );
 }
 
 export default App;
