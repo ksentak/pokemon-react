ae770e4874e976c956652a739b3f7cf0c1518774
KSentak <k.sentak@outlook.com>
2020-03-12 12:09:09 -0400

    Updates

---
+++ precache-manifest.dca845295dbcdc1064c7d9217fd3d85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff67eb3b32e4d7c91e5b1c842864b79",
+    "url": "/pokemon-react/index.html"
+  },
+  {
+    "revision": "87005c15b17194820136",
+    "url": "/pokemon-react/static/css/main.502d0e2c.chunk.css"
+  },
+  {
+    "revision": "08428b79690d405fdc9b",
+    "url": "/pokemon-react/static/js/2.cbeca34f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokemon-react/static/js/2.cbeca34f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87005c15b17194820136",
+    "url": "/pokemon-react/static/js/main.886a8476.chunk.js"
+  },
+  {
+    "revision": "d802c1968743e0bfbd98",
+    "url": "/pokemon-react/static/js/runtime-main.5bf0ffd4.js"
+  }
+]);
